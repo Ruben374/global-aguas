eed57a32885a5a096bf99362a75e27a208bcbd3c
Ruben André <rubandre14@gmail.com>
2023-06-11 16:07:17 +0100

    fisrt

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
 const Fade = require("react-reveal/Fade")
-const Hero2 = () => {
+import React from 'react';
+const Water = require('react-water-wave');
+export default function Hero() {
   return (
-    <div className='text-white bg-gradient-to-r from-cyan-500 h-screen to-blue-500'>
 
-      <Fade left>
-        <div className='max-w-[800px] w-full h-full mx-auto text-center flex flex-col justify-center'>
-          <div className='flex justify-center items-center'>
-            <p className='md:text-5xl sm:text-5xl text-xl font-bold py-4 text-white'>
-              Distribuição de água ao domicílio por cisterna
-            </p>
+    <div className="relative h-screen">
+      <div
+        className="absolute top-0 left-0 w-full h-full bg-[url('/hero-bg.jpg')] "
+        style={{ zIndex: -1 }}
+      ></div>
+      <div className="bg-blue-400 bg-opacity-50 w-full h-full flex flex-col justify-center items-center">
+        <Fade left>
+          <div className="max-w-[800px] w-full mx-auto text-center">
+            <div className="flex justify-center items-center">
+              <p className="md:text-5xl sm:text-5xl text-xl font-bold py-4 text-white">
+                Distribuição de água ao domicílio por cisterna
+              </p>
+            </div>
+            <p className="md:text-2xl text-xl text-white">Ficar sem água? Nunca mais.</p>
+            <button className="w-[170px] rounded-md font-medium my-6 mx-auto py-3 text-white border-2">
+              Começar
+            </button>
           </div>
-          <p className='md:text-2xl text-xl text-white'>Ficar sem água? Nunca mais.</p>
-          <button className='w-[170px] rounded-md font-medium my-6 mx-auto py-3 text-white border-2'>Começar</button>
-        </div>
-      </Fade>
+        </Fade>
+      </div>
     </div>
-  );
-};
 
-export default Hero2;
+
+  );
+}
